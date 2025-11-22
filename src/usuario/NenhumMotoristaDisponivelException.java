5d7544664ae694221761a781be800dde21761f9c
eliabe <74092305+eliabexp@users.noreply.github.com>
2025-11-22 00:46:30 -0300

    adiciona nova exception
    Co-authored-by: Gabriel Cardone <gabriell.cardone@gmail.com>
    Co-authored-by: Igor Dantas <igordnt.araujo@gmail.com>

---
+++ src/usuario/NenhumMotoristaDisponivelException.java
@@ -0,0 +1,7 @@
+package usuario;
+
+public class NenhumMotoristaDisponivelException extends RuntimeException {
+    public NenhumMotoristaDisponivelException(String message) {
+        super(message);
+    }
+}
