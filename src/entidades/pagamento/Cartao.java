97533f70608d2bd9855f54ff1afc37dcf912ac05
Igor Dantas <igordnt.araujo@gmail.com>
2025-12-01 22:38:13 -0300

    inicia fluxo de pagamento

--- src/entidades/pagamento/Cartao.java
+++ src/entidades/pagamento/Cartao.java
@@ -2,4 +2,8 @@ package entidades.pagamento;
 
 public abstract class Cartao extends FormaDePagamento {
     private String codigo;
+
+    public String getCodigo(){
+        return codigo;
+    }
 }
